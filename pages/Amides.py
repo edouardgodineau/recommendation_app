24ad932ea4229518391550cb0681422999f0d29f
Godineau Edouard CHST <edouard.godineau@syngenta.com>
2023-03-21 17:52:46 +0000

    Update Amides.py corrected import folder

--- pages/Amides.py
+++ pages/Amides.py
@@ -2,6 +2,6 @@
 # need to use Chemistry_recommendations.Utils.[filename] to make it work in pycharm
 
 #need to use Utils.[filename] to get the app to work within streamlit
-from Utils.amide_recommendations import show_page
+from chemistry_recommendations.amide_recommendations import show_page
 
 show_page()
